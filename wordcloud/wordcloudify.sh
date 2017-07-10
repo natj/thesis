83c374a1042b715b6df1d81563b9d480e32a34ee
natj <nattila.joonas@gmail.com>
2017-07-10 15:08:00 +0300

    update chapter banners

--- wordcloud/wordcloudify.sh
+++ wordcloud/wordcloudify.sh
@@ -1,1 +1,1 @@
-wordcloud_cli.py --text ../intro.tex --width 500 --height 150 --mask numbers/nw.001.png --background white --color darkgrey --stopwords ignored.txt --imagefile chap1b.png
+wordcloud_cli.py --text ../astro.tex --width 500 --height 150 --mask numbers/nw.003.png --background white --color darkgrey --stopwords ignored.txt --imagefile chap3b.png
